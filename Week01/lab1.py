d6732f0f7517a317ffca8aaa7c2dbc9321221b20
blossom-babs <saphashb@gmail.com>
2026-01-16 14:38:25 -0500

    add q5, lab1"

---
+++ Week01/lab1.py
@@ -0,0 +1,7 @@
+# Sample Coding Questions 01 Week 01
+# Blossom Babalola
+
+# Questuon 2: Defining an array (list)
+my_array = [1, 2, 3, 4, 5]
+
+
---
+++ Week01/q5.py
@@ -0,0 +1,3 @@
+userAge = input('Enter your age: ')
+userAge = int(userAge) + 22
+print(userAge)
